cc2ba1c9e844de91e4ea393c5cae1021d361b0b6
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-07-14 19:31:50 +0200

    style: comment line too long

--- 2026-07-02.js
+++ 2026-07-02.js
@@ -1,11 +1,15 @@
 /* Max Profit
 
-Given an array of daily stock prices and a budget (in dollars), calculate the maximum profit you could make by buying and selling the stock over the given period.
+Given an array of daily stock prices and a budget (in dollars),
+calculate the maximum profit you could make by buying and selling the stock
+over the given period.
 
  - You may only sell after you buy.
- - You may perform at most one buy and one sell transaction. Once you sell, you cannot buy again.
+ - You may perform at most one buy and one sell transaction.
+    Once you sell, you cannot buy again.
  - You can only buy whole shares.
- - Return the maximum possible profit as a string, rounded down to the nearest cent and formatted to two decimal places.
+ - Return the maximum possible profit as a string, rounded down to the
+    nearest cent and formatted to two decimal places.
 
 Tests:
 
