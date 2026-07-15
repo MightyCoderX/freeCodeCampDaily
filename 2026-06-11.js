9e598413ad33707f8a7a08ecc69311d7765d263b
MightyCoderX <36588044+MightyCoderX@users.noreply.github.com>
2026-07-15 10:18:44 +0200

    feat: solve june 11th challenge

---
+++ 2026-06-11.js
@@ -0,0 +1,25 @@
+/* Idea Rankings
+
+Given a 2D array where each inner array contains (in this order)
+an idea name, an optimistic estimate, a realistic estimate, and
+a pessimistic estimate (in days), return an array of the idea
+names sorted by expected time to completion, shortest first.
+
+Calculate the expected time to completion for each idea
+using the following formula:
+
+    expected = (
+        (optimistic + 4*realistic + pessimistic) / 6) *
+        length of idea name
+    )
+
+*/
+
+function analyzeIdeas(ideas) {
+    const expected = ([name, opt, real, pess]) =>
+        ((opt + 4 * real + pess) / 6) * name.length;
+
+    return ideas
+        .toSorted((a, b) => expected(a) - expected(b))
+        .map(row => row[0]);
+}
